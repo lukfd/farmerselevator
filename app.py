1cfdb77bf2a778ee3d6b377490a9099c7bc0e6e9
Daniel Taufiq <taufiqdanny@gmail.com>
2021-06-06 12:14:38 -0600

    mongodb with basic signup and signin

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, url_for, redirect, request
 import sqlite3 as lite
+import pymongo
+from pymongo import MongoClient
 from random import randint
+import json
+from bson import json_util
 
 app = Flask(__name__)
+# mongodb code
+cluster = MongoClient("mongodb+srv://farmer:tiger22@cluster0.ymdax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+db = cluster["FarmersElevator"]
+
+
+
 
 @app.route('/')
 def index():
@@ -12,34 +23,34 @@ def index():
 def signin():
     return '''
     <h1>Farmers & Elevators</h1>
-	<form action="/signin-form" method="post">
-		<label>Username:</label>
-		<input type="text" name="username"/>
-		<label>Password:</label>
-		<input type="password" name="password"/>
-		<label>elevator?</label>
-		<input type="checkbox" name="elevator"/>
-		<button type="submit">sign-in</button>
-	</form>
+    <form action="/signin-form" method="post">
+        <label>Username:</label>
+        <input type="text" name="username"/>
+        <label>Password:</label>
+        <input type="password" name="password"/>
+        <label>elevator?</label>
+        <input type="checkbox" name="elevator"/>
+        <button type="submit">sign-in</button>
+    </form>
     '''
 
 @app.route('/signup')
 def signup():
     return '''
     <h1>Farmers & Elevators</h1>
-	<form action="/signup-form" method="post">
-		<label>Email:</label>
-		<input type="email" name="email"/>
-		<label>Username:</label>
-		<input type="text" name="username"/>
-		<label>Password:</label>
-		<input type="password" name="password"/>
-		<label>retype Password:</label>
-		<input type="password"/>
-		<label>elevator?</label>
-		<input type="checkbox" name="elevator"/>
-		<button type="submit">sign-up</button>
-	</form>
+    <form action="/signup-form" method="post">
+        <label>Email:</label>
+        <input type="email" name="email"/>
+        <label>Username:</label>
+        <input type="text" name="username"/>
+        <label>Password:</label>
+        <input type="password" name="password"/>
+        <label>retype Password:</label>
+        <input type="password"/>
+        <label>elevator?</label>
+        <input type="checkbox" name="elevator"/>
+        <button type="submit">sign-up</button>
+    </form>
     '''
 
 # parameters: username, password, elevator
@@ -52,54 +63,43 @@ def signin_form():
     # if it is an Elevator logging in
     if isElevator == "on":
         # check username and password in DB
-        con = lite.connect('base.db') 
-        cur = con.cursor()
-        cur.execute(f"SELECT username from elevators WHERE username='{username}' AND Password = '{password}';")
-        if not cur.fetchone():  # An empty result evaluates to False.
-            cur.close()
-            return '''
-                <h1>Farmers & Elevators</h1>
-                <h3>Login Failed, try again</h3>
-                <form action="/signin-form" method="post">
-                    <label>Username:</label>
-                    <input type="text" name="username"/>
-                    <label>Password:</label>
-                    <input type="password" name="password"/>
-                    <label>elevator?</label>
-                    <input type="checkbox" name="elevator"/>
-                    <button type="submit">sign-in</button>
-                </form>
-                '''
-        else:
-            # create new session
-
-            # redirect to main page
-            return redirect("/home", code=302) 
-    else:
-        # check username and password in DB
-        con = lite.connect('base.db') 
-        cur = con.cursor()
-        cur.execute(f"SELECT username from farmers WHERE username='{username}' AND Password = '{password}';")
-        if not cur.fetchone():  # An empty result evaluates to False.
-            cur.close()
-            return '''
-                <h1>Farmers & Elevators</h1>
-                <h3>Login Failed, try again</h3>
-                <form action="/signin-form" method="post">
-                    <label>Username:</label>
-                    <input type="text" name="username"/>
-                    <label>Password:</label>
-                    <input type="password" name="password"/>
-                    <label>elevator?</label>
-                    <input type="checkbox" name="elevator"/>
-                    <button type="submit">sign-in</button>
-                </form>
-                '''
-        else:
-            # create new session
-            
+        collection = db["Elevators"]
+        if collection.find_one({"name": username, "password": password}):
             # redirect to main page
             return redirect("/home", code=302)
+        else:
+            return '''
+                 <h1>Farmers & Elevators</h1>
+                 <h3>Login Failed, try again</h3>
+                 <form action="/signin-form" method="post">
+                     <label>Username:</label>
+                     <input type="text" name="username"/>
+                     <label>Password:</label>
+                     <input type="password" name="password"/>
+                     <label>elevator?</label>
+                     <input type="checkbox" name="elevator"/>
+                     <button type="submit">sign-in</button>
+                 </form>
+                 '''
+    else:
+        collection = db["Farmers"]
+        if collection.find_one({"name": username, "password": password}):
+            return redirect("/home", code=302)
+        else:
+            return '''
+                 <h1>Farmers & Elevators</h1>
+                 <h3>Login Failed, try again</h3>
+                 <form action="/signin-form" method="post">
+                     <label>Username:</label>
+                     <input type="text" name="username"/>
+                     <label>Password:</label>
+                     <input type="password" name="password"/>
+                     <label>elevator?</label>
+                     <input type="checkbox" name="elevator"/>
+                     <button type="submit">sign-in</button>
+                 </form>
+                 '''
+
 
 # Parameters: email, username, password, elevator
 @app.route('/signup-form', methods=['POST'])
@@ -108,43 +108,30 @@ def signup_form():
     email = request.form['email']
     username = request.form['username']
     password = request.form['password']
-    toInsert = ('name', randint(0, 100000), email, password, username, 1,)
-    print(len(toInsert))
 
-    if isElevator == "on":
-        con = lite.connect('base.db') 
-        cur = con.cursor()
+    if(isElevator == "on"):
         #create the new profile into the database
         try:
-            cur.execute("""INSERT INTO elevators
-                            (name, elevator_id, email, password, username, status) 
-                            VALUES (?, ?, ?, ?, ?, ?);""", toInsert)
-            con.commit()
-            cur.close()
+            collection = db["Elevators"]
+            collection.insert_one({"name": username, "email": email, "password": password, 
+                                    "primary contact": None, "phone": None, "website": None, 
+                                    "shop_url": None, "address": None,  "profile_img": None,"status": 1})
             # redirect to sign in page
             return redirect("/signin", code=302)
-        except lite.Error as error:
-            return "Failed "+str(error)
-        finally:
-            if (con):
-                con.close()
+        except ConnectionFailure:
+            print("Server is not available")
     else:
-        con = lite.connect('base.db') 
-        cur = con.cursor()
         # create the new profile into the database
         try:
-            cur.execute("""INSERT INTO farmers
-                            (name, farmer_id, email, password, username, status) 
-                            VALUES (?, ?, ?, ?, ?, ?);""", toInsert)
-            con.commit()
-            cur.close()
+            collection = db["Farmers"]
+            collection.insert_one({"lastname": None, "name": username, "email:": email, 
+                                    "password": password, "phone": None, "address": None, 
+                                    "profile_img": None, "status": 1})
             # redirect to sign in page
             return redirect("/signin", code=302)
-        except lite.Error as error:
-            return "Failed "+str(error)
-        finally:
-            if (con):
-                con.close()
+        except ConnectionFailure:
+            print("Server is not available")
+
 
 @app.route('/home')
 def homepage():
