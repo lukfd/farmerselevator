d5dea39a926ea6e5af2dfc5f871c98330b55aa58
lukfd <luca.comba98@gmail.com>
2021-09-28 23:14:50 -0500

    fixing index search box functionality with Vue

--- farmerselevator/static/js/index.js
+++ farmerselevator/static/js/index.js
@@ -3,13 +3,32 @@
 // 
 
 // GLOBAL VARIABLES
+var app;
 var mymap;
+var elevatorList;
 // Minnesota Center Map
 var lng = -94.648311
 var lat = 46.108537
 
 // when page loads 
 function init() {
+
+    $.get('/getElevatorList', (data) => {
+        elevatorList = data
+    })
+
+    // Vue
+    app = new Vue({
+        el: '#app',
+        data: {
+          searchInput: '',
+          results: [],
+          discoveredResultsNames:[]
+        },
+        methods: {
+            filterSearch: filterSearch
+        }
+    })
 
     // Map
     // SETTING UP MAP
@@ -27,4 +46,28 @@ function init() {
         //bounds: mybounds,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(mymap);
+}
+
+
+//---
+// @return: add what it finds to app.results
+// @paramaters:
+//      app.searchInput is what the user inputted
+//      elevatorList is the full list of elevators
+//---
+function filterSearch () {
+
+    if (app.searchInput === ' ' || app.searchInput === '') {
+        app.results = []
+        app.discoveredResultsNames = []
+    }
+
+    elevatorList.forEach((obj) => {
+        // if is the same as inputted
+        if (obj.name.toUpperCase().includes(app.searchInput.toUpperCase()) &&
+         app.searchInput !== '' && app.searchInput !== ' ' &&  !app.discoveredResultsNames.includes(obj.name)) {
+            app.results.push({name: obj.name, link: '/shop?name='+obj.name})
+            app.discoveredResultsNames.push(obj.name)
+        }
+    });
 }
