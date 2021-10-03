d7d175e509d19b37605f47d6d621edf020a0cfbb
lukfd <luca.comba98@gmail.com>
2021-10-02 19:22:25 -0500

    fixed profile by adding profileType

--- farmerselevator/src/pages/profile.py
+++ farmerselevator/src/pages/profile.py
@@ -6,18 +6,31 @@ from flask import render_template
 from flask import escape
 
 
-@application.route('/profile/<string:id>', methods=['GET'])
-def profile(id):
+@application.route('/profile/<string:profileType>/<string:id>', methods=['GET'])
+def profile(profileType,id):
     loggedin = False
     if 'username' in session:
         loggedin = True
-    # select from the table
+    #connect to database
     con = lite.connect(farmerselevator.constants.databasePath) 
     cur = con.cursor()
-    cur.execute(f"SELECT username, primary_contact, phone, email, address, profile_image from elevators WHERE elevator_id='{id}';")
-    result = cur.fetchone()
-    if not result:  # An empty result evaluates to False.
-        cur.execute(f"SELECT username, name, lastname, email, profile_image from farmers WHERE farmer_id='{id}';")
+    if profileType == 'elevator':
+        cur.execute(f"SELECT username, primary_contact, phone, email, address, profile_image from elevators WHERE elevator_id=?;", (id,))
+        result = cur.fetchone()
+        if not result:
+            cur.close()
+            return f'''
+            <h1>Farmers & Elevators</h1>
+            <nav>
+                <h3>Account {escape(id)} not found</h3>
+                <a href="/signup">create a new account</a>
+                <a href="/">home</a>
+            </nav>
+            '''
+        cur.close()
+        return render_template('profile.html', loggedin=loggedin, username=result[0], profile_type = "elevator", name=result[1], phone=result[2], email=result[3], address=result[4], image=str(result[5]))
+    elif profileType == 'farmer':
+        cur.execute(f"SELECT username, name, lastname, email, profile_image from farmers WHERE farmer_id=?;", (id,))
         result = cur.fetchone()
         result = ['' if x is None else x for x in result]
         if not result:
@@ -30,9 +43,5 @@ def profile(id):
                 <a href="/">home</a>
             </nav>
             '''
-        else:
-            cur.close()
-            return render_template('profile.html', loggedin=loggedin, username=result[0], profile_type = "farmer", name=result[1]+' '+result[2], email=result[3], image=str(result[4]))    
-    else:
         cur.close()
-        return render_template('profile.html', loggedin=loggedin, username=result[0], profile_type = "elevator", name=result[1], phone=result[2], email=result[3], address=result[4], image=str(result[5]))
+        return render_template('profile.html', loggedin=loggedin, username=result[0], profile_type = "farmer", name=result[1]+' '+result[2], email=result[3], image=str(result[4]))
