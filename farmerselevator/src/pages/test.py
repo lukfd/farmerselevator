568cc26038fc8174157edfb75740a646ac7a0605
lukfd <luca.comba98@gmail.com>
2022-03-23 23:50:05 -0500

    comment out test page to send email

--- farmerselevator/src/pages/test.py
+++ farmerselevator/src/pages/test.py
@@ -8,10 +8,10 @@ from flask import render_template
 @application.route('/test')
 def test():
 
-    message = "This was sent by Luca"
-    serverSmtp = smtplib.SMTP("smtp.gmail.com", 587)
-    serverSmtp.starttls()
-    serverSmtp.login(os.getenv('EMAIL_ADDRESS'), os.getenv('EMAIL_PASSWORD'))
-    serverSmtp.sendmail(os.getenv('EMAIL_ADDRESS'), "luca.comba98@gmail.com", message)
+    # message = "This was sent by Luca"
+    # serverSmtp = smtplib.SMTP("smtp.gmail.com", 587)
+    # serverSmtp.starttls()
+    # serverSmtp.login(os.getenv('EMAIL_ADDRESS'), os.getenv('EMAIL_PASSWORD'))
+    # serverSmtp.sendmail(os.getenv('EMAIL_ADDRESS'), "luca.comba98@gmail.com", message)
 
     return render_template('test.html')
